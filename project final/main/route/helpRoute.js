bbd033c0a6ee364c994612b87517732c1b82acdd
vivekbhupathi9 <vivek.bhupathi@nineleaps.com>
2021-01-31 18:27:32 +0530

    folder structure change

--- project final/main/route/helpRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require("express-promise-router")();
-const help = require('../controller/helpController')
-
-router.route('/addHelp').post(help.addHelp)
-router.route('/modifyHelp').patch(help.modifyHelp)
-router.route('/deleteHelp').delete(help.deleteHelp)
-router.route('/getAllHelp').get(help.getAllHelp)
-
-module.exports = router;
